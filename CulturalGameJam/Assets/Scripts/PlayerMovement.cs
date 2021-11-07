37d1614ef3b56f14a44f5b39e93f2164b73a8393
JDiegoS <46642689+JDiegoS@users.noreply.github.com>
2021-11-07 02:16:14 -0600

    Movimiento inicial

---
+++ CulturalGameJam/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float movementX;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] public float speed = 5f;
+    [SerializeField] public float jumpf = 5f;
+    private bool grounded;
+    //[SerializeField] private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movementX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(movementX * speed, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpf);
+            grounded = false;
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "ground")
+            grounded = true;
+    }
+
+}
