8d33789adf572d9a9773d20c2107c7a6cd533894
Arem58 <46797233+Arem58@users.noreply.github.com>
2021-11-20 20:38:56 -0600

    Revert "Avances de las luces y collider de personaje"
    This reverts commit edcf00754d7a7c6b46635b415de2cb58948e274c.

--- CulturalGameJam/Assets/Scripts/Lights/PlayerIntensity.cs
+++ CulturalGameJam/Assets/Scripts/Lights/PlayerIntensity.cs
@@ -21,7 +21,6 @@ public class PlayerIntensity : MonoBehaviour
         playerLight.GetComponent<Light2D>();
         playerLight.pointLightOuterRadius = 1.18f;
         playerLight.pointLightInnerRadius = 0f;
-        playerLight.enabled = false;
     }
     
     public void LightIntensity()
